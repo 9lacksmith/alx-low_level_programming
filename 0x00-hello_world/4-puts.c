ea5fc013dcabc68f613d8996a84151a6e19b20d7
9lackmsith <panashewhispermatarutse@hotmail.com>
2022-09-12 22:43:49 +0200

    C is now commited

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Printing
+ *
+ * Reuturn: Always 0 (Successful)
+ */
+int main(void)
+{
+	puts("\"Programming  is like building a multilingual puzzle");
+	return (0);
+}
