033ca0f122322e58ffd258ec21ddfce1731bd881
9lackmsith <panashewhispermatarutse@hotmail.com>
2022-11-08 23:59:53 +0200

    puts correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 #include <stdio.h>
 
 /**
@@ -9,7 +7,7 @@
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
+		puts("Programming is like building a multilingual puzzle");
+			return (0);
 
 }
