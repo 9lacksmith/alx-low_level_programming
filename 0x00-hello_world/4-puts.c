bcfe245aa77de080ca5015e46624cd7ad4c0a553
Panashe Whisper Matarutse <panashewhispermatarutse@hotmail.com>
2023-08-11 01:08:11 +0200

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints"Programming is like buildinga multilingual puzzle" 
+ *
+ * Return: Always 0(success)
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);	 
+}
