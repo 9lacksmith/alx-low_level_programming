b8ac8b77ffddf25d1cc783a0237cf14738e214e8
Panashe Whisper Matarutse <panashewhispermatarutse@hotmail.com>
2023-08-11 01:15:57 +0200

    corrections for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints"Programming is like buildinga multilingual puzzle" 
- *
+ * main - Prints "Programming is like building a multilingual puzzle"
  * Return: Always 0(success)
  */
 
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);	 
+	return (0);
 }
