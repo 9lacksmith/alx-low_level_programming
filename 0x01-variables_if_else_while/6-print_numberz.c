5d12f6dda2f2eaa7f41473033fa616917e4afa7d
Panashe Whisper Matarutse <panashewhispermatarutse@hotmail.com>
2023-08-12 02:53:06 +0200

    Answering task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit number of base 10 starting from 0
+ *	- followed by new line
+ *
+ * Return: Always 0 (success)
+ */
+
+ int main (void)
+ {
+	 int num;
+
+	 num = 48;
+
+	 while (num < 58)
+	 {
+		 putchar(num);
+		 num++;
+	 }
+	 putchar('\n');
+	 return(0);
+ }
