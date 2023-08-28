0f818b8c5f040248d4f5ed4787c4bf4bc52c0d7b
Panashe Whisper Matarutse <panashewhispermatarutse@hotmail.com>
2023-08-28 10:38:57 +0200

    Task 3 , should print is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks for the lowercase character
+ *	Return: 1 if c is lower case 
+ *	Reurn : 0 otherwise 
+ */
+
+int _islower(int c )
+{
+	return (c >= 'a' && c <= 'z'); 
+}
