00a075578e00bc6fb55a73caa1fa85749fcda9af
Panashe Whisper Matarutse <panashewhispermatarutse@hotmail.com>
2023-09-22 16:19:30 +0200

    should work now

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int ab);
 void jack_bauer(void);
-void times_table(void);`
+void times_table(void);
 int print_last_digit(int nld);
 int add(int, int);
 void print_to_98(int n);
