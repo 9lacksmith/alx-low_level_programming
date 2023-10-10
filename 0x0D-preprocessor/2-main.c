dab4e22845b5d0e97317ff90f2ed29db240dc050
Panashe Whisper Matarutse <panashewhispermatarutse@hotmail.com>
2023-10-10 21:52:21 +0200

    All the tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the file that was compiled
+ * Return: Int
+ */
+
+int main(void)
+{
+	_putchar(__FILE__);
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <mathlib.h>
+
+#define ABS(x) (x > 0 ? (x) : (x) * (-1))
