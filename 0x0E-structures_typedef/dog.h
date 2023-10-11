665dde08064e452826281ad7eec0a868d7e0fe59
Panashe Whisper Matarutse <panashewhispermatarutse@hotmail.com>
2023-10-11 05:04:27 +0200

    this is for task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,10 +7,12 @@
  * @owner: charcter string
  */
 
-typedef struct dog 
+typedef struct dog
 {
 	char *name;
 	float *age;
 	char *owner;
 } dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
