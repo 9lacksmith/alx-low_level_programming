3ccc43d89aca0ba2539be22f1d52dcf0752f0ed1
Panashe Whisper Matarutse <panashewhispermatarutse@hotmail.com>
2023-10-11 04:36:28 +0200

    The first task

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+ This directory contains the task for 0x0E-structures & typedef
+ There are 6 tasks in total.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @owner: charcter string
+ */
+
+struct dog 
+{
+	char *name;
+	float *age;
+	char *owner;
+} ;
+#endif
