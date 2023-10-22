00586e224e4dc4851cb69abb067b6f6828d06ee0
Panashe W Matarutse <panashewhispermatarutse@hotmail.com>
2023-10-22 20:54:12 +0200

    The tasks for typedef and structs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,9 +10,15 @@
 typedef struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 } dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
